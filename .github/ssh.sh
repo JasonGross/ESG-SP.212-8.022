42a8e8ca0ad40072b8d6f7f7d83c901b33d1573e
Jason Gross <jgross@mit.edu>
2011-02-06 15:03:36 -0500

    Fixed ssh script.

--- .github/ssh.sh
+++ .github/ssh.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Absolute path to this script. /home/user/bin/foo.sh
-SCRIPT=$(readlink -f $0)
+SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in. /home/user/bin
 SCRIPTPATH=`dirname "$SCRIPT"`
 CURRENTDIR=$PWD
