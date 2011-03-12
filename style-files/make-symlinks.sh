095e039f8f04d743d7ef35ea4be2c6eea5cceaca
Jason Gross <jgross@mit.edu>
2011-03-12 03:32:06 -0500

    Updated make-symlinks file.

--- style-files/make-symlinks.sh
+++ style-files/make-symlinks.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-git update-index --assume-unchanged *.{sty,dtx,tex,cmap,fd,ins}
+git update-index --assume-unchanged *
+git update-index --no-assume-unchanged *.sh
 for i in *; do if [ -f ../../../Style\ Files/$i ]; then echo $i; ln -sf ../../../Style\ Files/$i ./; fi; done
