e271c64a8e6c9169ea10f9ccef5486b6f36b5bab
Jason Gross <jgross@mit.edu>
2011-03-12 02:27:11 -0500

    Removed file to make symlinks.

--- style-files/make-symlinks.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-git update-index --assume-unchanged *.{sty,dtx,tex,cmap,fd,ins}
-for i in *; do if [ -f ../../../Style\ Files/$i ]; then echo $i; ln -sf ../../../Style\ Files/$i ./; fi; done
