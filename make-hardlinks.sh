5da03d24508433428f0f5d63d12c7e246d69268c
Jason Gross <jgross@mit.edu>
2011-03-17 17:27:06 -0400

    Updated some scripting files.

---
+++ make-hardlinks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in. /home/user/bin
+SCRIPTPATH=`dirname "$SCRIPT"`
+
+pushd $SCRIPTPATH
+git update-index --assume-unchanged *.{sty,cmap,def}
+for i in *.{sty,cmap,def}; do if [ -f style-files/$i ]; then echo $i; ln -f style-files/$i ./; fi; done
+
+cd style-files
+git update-index --assume-unchanged *
+git update-index --no-assume-unchanged *.sh
+for i in *; do if [ -f ../../../Style\ Files/$i ]; then echo $i; ln -f ../../../Style\ Files/$i ./; fi; done
+popd
--- style-files/make-symlinks.sh
+++ style-files/make-symlinks.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in. /home/user/bin
+SCRIPTPATH=`dirname "$SCRIPT"`
+
+pushd $SCRIPTPATH
 git update-index --assume-unchanged *
 git update-index --no-assume-unchanged *.sh
 for i in *; do if [ -f ../../../Style\ Files/$i ]; then echo $i; ln -sf ../../../Style\ Files/$i ./; fi; done
+popd
